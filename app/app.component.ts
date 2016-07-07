75b00c133270497342df510b00d20d100681c639
wayko <programmers@dreamcpu.com>
2016-07-06 22:35:32 -0400

    first Angular 2 app
    first Angular 2 app

---
+++ app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+template: `<h1>{{title}}</h1>
+		<h2>{{carPart.name}}</h2>
+		<p>{{carPart.description}}</p>
+		<p>{{carPart.inStock}}</p>`
+})
+
+export class AppComponent { 
+	title = 'Ultra Racing2';
+	carPart = {
+		"id": 1,
+		"name": "Super Tires",
+		"description": "These tires are the very best",
+		"inStock": 2
+	};
+}
